9839d3f5632f54a538b5e8e88644553f4381f674
Brittany <brittany@basecampcodingacademy.org>
2025-04-15 12:44:27 -0500

    updating styles

--- app/views.py
+++ app/views.py
@@ -16,6 +16,10 @@ from django.contrib.auth.models import Group
 
 # Main Pages ----
 
+def home_view(request):
+   context = {}
+   return render(request, "index.html", context)
+
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['employee', 'admin'])
 def all_data_view(request):
